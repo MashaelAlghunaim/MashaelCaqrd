7e84e7689a585aa2d1c26bba0d333782a1416e2a
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-28 17:15:08 +0300

    Initial Commit

---
+++ MashaelCaqrd/MashaelCaqrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  MashaelCaqrdApp.swift
+//  MashaelCaqrd
+//
+//  Created by Mashael Alghunaim on 03/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MashaelCaqrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
